9a3130833667ac30b30792808f8754e8b87deff8
JerryChowMx <jerrychowmx@gmail.com>
2025-12-17 11:25:18 -0600

    fix(comments): update controller with correct Strapi v5 documentService syntax

--- magnus-strapi/src/api/comment/controllers/comment.ts
+++ magnus-strapi/src/api/comment/controllers/comment.ts
@@ -1,7 +1,29 @@
 /**
- * comment controller - default
+ * comment controller
  */
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::comment.comment');
+export default factories.createCoreController('api::comment.comment', ({ strapi }) => ({
+    async find(ctx) {
+        // Use documentService with correct Strapi v5 syntax
+        const documents = await strapi.documents('api::comment.comment').findMany({
+            filters: ctx.query.filters,
+            sort: ctx.query.sort,
+            populate: ['author', 'parent', 'article'],
+            pagination: ctx.query.pagination
+        });
+
+        return {
+            data: documents,
+            meta: {
+                pagination: {
+                    page: 1,
+                    pageSize: 25,
+                    pageCount: 1,
+                    total: documents.length
+                }
+            }
+        };
+    }
+}));
